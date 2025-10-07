27acd78e2c6e658537934015c21a949393936fd5
Albi Taulla <albi.taulla@sap.com>
2025-10-07 19:42:05 +0200

    Add new reranking library

--- src/components/models/reranker_facade.py
+++ src/components/models/reranker_facade.py
@@ -1,25 +1,24 @@
-from colbert.infra import Run, RunConfig
-from colbert import Rerank
 from typing import List, Dict, Any
+from colbert import ColbertReranker
 
 class Reranker:
-    def __init__(self):
-        self.reranker = Rerank()
+    def __init__(self, checkpoint='colbert-ir/colbertv2.0'):
+        self.reranker = ColbertReranker(checkpoint=checkpoint)
 
     def rerank(self, query: str, documents: List[Dict[str, Any]], k: int) -> List[Dict[str, Any]]:
         """
-        Reranks a list of documents based on a query.
+        Reranks a list of documents based on a query using Colbert v2.
 
         Args:
             query: The query string.
-            documents: A list of documents to rerank.
+            documents: A list of documents to rerank. Each document is a dictionary.
+            k: The number of documents to return.
 
         Returns:
             A list of reranked documents.
         """
         doc_texts = [doc["description"] for doc in documents]
         
-        with Run().context(RunConfig(nranks=1, experiment="rerank")):
-            reranked_indices = self.reranker.rerank(query, doc_texts)
+        reranked_indices = self.reranker.rerank(query, doc_texts)
         
         return [documents[i] for i in reranked_indices][:k]
