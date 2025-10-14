d7ef58beb949f1fcc065f5bbd9c57e28873051da
Albi Taulla <albi.taulla@sap.com>
2025-10-14 15:56:19 +0200

    Add dev dataset

--- src/init.py
+++ src/init.py
@@ -20,7 +20,7 @@ def main():
     """
     # load_comments()
 
-    dataset_path = "/var/tmp/ge62nok/thesis/dataset/dev/bird_subset.json"
+    dataset_path = "/var/tmp/ge62nok/thesis/dataset/dev/dev.json"
     manager = RunningManager(dataset_path)
     manager.load_tasks()
     manager.run_evaluation()
